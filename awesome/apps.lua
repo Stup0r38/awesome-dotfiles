e67ee672d04afc51e6d3783f566824a07f9acf4f
William McKinnon <willpower3309@gmail.com>
2020-01-30 22:01:59 -0500

    picom blurring now works

--- awesome/apps.lua
+++ awesome/apps.lua
@@ -31,7 +31,7 @@ local apps = {
 
 -- List of apps to start once on start-up
 local run_on_start_up = {
-    "compton",
+    "picom --experimental-backends",
     "redshift",
     "unclutter",
     "flashfocus"
