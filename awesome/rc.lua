07d40164a13facfcd04872f59aa2ba49a3b95787
Alessandro Princiotto <alessandrop@fedoraws.lan>
2020-07-01 21:11:25 +0200

    removed borders when window is maximized

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -107,6 +107,19 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
+--Remove borders when window is maximized
+screen.connect_signal("arrange", function (s)
+    local max = s.selected_tag.layout.name == "max"
+    local only_one = #s.tiled_clients == 1 -- use tiled_clients so that other floating windows don't affect the count
+    -- but iterate over clients instead of tiled_clients as tiled_clients doesn't include maximized windows
+    for _, c in pairs(s.clients) do
+        if (max or only_one) and not c.floating or c.maximized then
+            c.border_width = 0
+        else
+            c.border_width = beautiful.border_width
+        end
+    end
+end)
 
 -- ===================================================================
 -- Garbage collection (allows for lower memory consumption)
